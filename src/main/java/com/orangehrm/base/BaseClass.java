7bb45a5faf84180d7737f809ee58692198b8a8c0
rajaprasath21 <rajaprasath21@gmail.com>
2026-02-01 20:32:45 +0530

    Update BaseClass.java
    Headless issue fixed now and maiximize browser disabled

--- src/main/java/com/orangehrm/base/BaseClass.java
+++ src/main/java/com/orangehrm/base/BaseClass.java
@@ -130,11 +130,11 @@ public class BaseClass {
 			*/
 			
 			// ✅ Use new headless mode (VERY IMPORTANT)
-			//options.addArguments("--headless");
+			options.addArguments("--headless");
 			//options.addArguments("--guest");
 
 			// ✅ Explicitly set window size
-			//options.addArguments("--window-size=1920,1080");
+			options.addArguments("--window-size=1920,1080");
 
 			// ✅ Stability flags
 			options.addArguments("--disable-gpu");
@@ -144,7 +144,7 @@ public class BaseClass {
 			
 			//WebDriverManager.chromedriver().setup();
 			//driver=new ChromeDriver();
-			driver.set(new ChromeDriver());  //New changes as per Threadlocal
+			driver.set(new ChromeDriver(options));  //New changes as per Threadlocal
 			//Register WebDiver for current Thread for ExtentReporting
 			ExtentManager.registerDriver(getDriver());
 			logger.info("ChromeDriver Instance created.");
@@ -199,7 +199,7 @@ public class BaseClass {
 		getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(implicitWait));
 
 		//Maximize the browser
-		getDriver().manage().window().maximize();
+		//getDriver().manage().window().maximize();
 
 		//Navigate to URL
 		String url;
